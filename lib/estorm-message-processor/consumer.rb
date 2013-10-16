f61f442b35bc974cf9a0226dd86fcd50b8624d6e
semdinsp <scott.sproule@ficonab.com>
2013-10-16 10:56:19 +0800

    more fixes for consumer improvements

---
+++ lib/estorm-message-processor/consumer.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'bunny'
+require 'yaml'
+
+module EstormMessageProcessor
+  class Consumer  < Bunny::Consumer
+    attr_accessor :logger
+    def cancelled?
+      @cancelled
+    end
+    def count
+        @mycount=0  if @mycount==nil
+        @mycount
+    end
+    def increment
+       @mycount=self.count+1
+    end
+
+    def handle_cancellation(_)
+      
+      msg="consumer cancellation queue called"
+      @logger.info msg
+      @cancelled = true
+    end
+    # process message
+    def process_messages(delivery_info,metadata,body)
+       begin
+         cmdhash=YAML.load(body)
+         delegatestring="delegate_#{cmdhash['command']}"
+         # create procedures named delegate_command accepting cmdhash
+         msg = "-----> [gem estorm message processor:  consumer] Received from App #{body} calling delegate #{delegatestring} count: #{self.count} "
+         @logger.info msg
+         self.send(delegatestring,cmdhash)
+         # self.send(delegatestring,cmdhash)
+         #load the promotion and process through data
+         self.increment
+       rescue  Exception => e    # add could not convert integer
+         msg= "[gem estorm message processor] bunny exception #{e.message} found #{e.inspect} #{e.backtrace}..."  #THIS NEEDS WORK!
+         @logger.info msg
+
+        end
+     end
+    # get the queue statistics and log it.
+    def queue_statistics
+      msg_count=0
+      begin
+      msg_count =@queue.message_count
+      consumer_count =@queue.consumer_count
+      msg = "queue status for queue [#{@queue.name}] message count: #{msg_count} consumers: #{consumer_count}"
+      @logger.info msg if @logger!=nil
+      rescue  Exception => e
+        msg = "exception in queue statistics #{e.inspect}"
+        @logger.info msg  if @logger!=nil
+      end
+      [msg_count, consumer_count]
+     end
+  end
+end
